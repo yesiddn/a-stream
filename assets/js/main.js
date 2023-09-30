26c4479efcbe6f639c1b73a47314cb7a90277fdd
Yesid <yesidrodriguez305@gmail.com>
2023-09-29 20:18:03 -0500

    Fix duplicate data loading error

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,8 @@ async function getTrendingMoviesPreview() {
   const { data } = await api(`/trending/movie/day`);
 
   const movies = data.results;
+  trendingMoviesPreviewList.innerHTML = '';
   movies.forEach((movie) => {
-    const trendingMoviesPreviewList = document.querySelector(
-      '#trendingPreview .trendingPreview-movieList'
-    );
 
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -39,10 +37,8 @@ async function getCategoriesPreview() {
   const { data } = await api(`/genre/movie/list`);
 
   const categories = data.genres;
+  categoriesPreviewList.innerHTML = '';
   categories.forEach((category) => {
-    const categoriesPreviewList = document.querySelector(
-      '#categoriesPreview .categoriesPreview-list'
-    );
 
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
